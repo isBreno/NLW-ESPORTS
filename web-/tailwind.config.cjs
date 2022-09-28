c002513a399c9186fe08c39bbe8b8b033ddb3b02
Breno <eusoubrenovieira@gmail.com>
2022-09-28 12:08:47 -0300

    changed background format to svg

--- web-/tailwind.config.cjs
+++ web-/tailwind.config.cjs
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')",
+        galaxy: "url('/background-galaxy.svg')",
         nlw_gradient:
           "linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)",
         game_shadow_gradient:
